348130bc8462fd979381bd8230eb8564e74b87ab
tochratana <ltomusic69@gmail.com>
2026-01-08 11:36:35 +0700

    Update: 2026-01-08 11:36:35

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
 
 
-  const username = "postgres";
-  const passowrd  = "EreifsF&3";
-  const port = 5432;
+  // const username = "postgres";
+  // const passowrd  = "EreifsF&3";
+  // const port = 5432;
 
-  const hello = ()=>{
-    console.log(username);
-    console.log(passowrd);
-    console.log(port)
-  }
+  // const hello = ()=>{
+  //   console.log(username);
+  //   console.log(passowrd);
+  //   console.log(port)
+  // }
   return (
     <div className="w-full">
       {/* Hero Section */}
